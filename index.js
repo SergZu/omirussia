660456b7718f9831ec47d1415ccff6c0210a6f22
Oleg Morshel <Morshel@addamant.ru>
2022-06-22 15:44:06 +0300

    Fix adaptive styles and add counter

---
+++ index.js
@@ -0,0 +1,15 @@
+const target = document.querySelector(".digits");
+const initValue = "432524";
+let value = initValue;
+for (let i = 1; i < initValue.length + 1; i++) {
+  const target = document.querySelector(`.digits-${i}`);
+  target.style.marginTop = `-${initValue[i - 1]}em`;
+}
+setInterval(() => {
+  const newValue = `${Number(value) + Math.round(Math.random() * 3 + 2)}`;
+  value = newValue;
+  for (let i = 1; i < newValue.length + 1; i++) {
+    const target = document.querySelector(`.digits-${i}`);
+    target.style.marginTop = `-${newValue[i - 1]}em`;
+  }
+}, 2500);
